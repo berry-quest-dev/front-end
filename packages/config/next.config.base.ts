7a8e9e2d28f39761d623b19d91396e1e29278a66
Lee Ye Seo <rosefor2ne@gmail.com>
2025-03-25 07:12:41 +0900

    fix: packages/ui 못찾는 에러 해결

--- packages/config/next.config.base.ts
+++ packages/config/next.config.base.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import type { NextConfig } from "next";
 import withTM from "next-transpile-modules";
 
@@ -5,6 +6,35 @@ const withTranspile = withTM(["@packages/ui"]);
 
 const baseConfig: NextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@packages/ui": path.resolve(__dirname, "../../packages/ui/src"),
+      "@packages/api": path.resolve(__dirname, "../../packages/api"),
+      "@packages/utils": path.resolve(__dirname, "../../packages/utils"),
+    };
+
+    return config;
+  },
 };
 
 export default withTranspile(baseConfig);
+
+
+// import withTM from "next-transpile-modules";
+
+// const nextConfig = withTM(["@packages/ui"])({
+//   reactStrictMode: true,
+//   webpack: (config) => {
+//     config.resolve.alias = {
+//       ...config.resolve.alias,
+//       react: require.resolve("react"),
+//       "react-dom": require.resolve("react-dom"),
+//       "react/jsx-runtime": require.resolve("react/jsx-runtime"),
+//       "react/jsx-dev-runtime": require.resolve("react/jsx-dev-runtime"),
+//     };
+//     return config;
+//   },
+// });
+
+// export default nextConfig;
